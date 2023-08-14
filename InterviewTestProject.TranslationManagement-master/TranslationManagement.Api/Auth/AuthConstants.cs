b475bf3b72b34fd6c17f4b011ed2217d52c732ef
Zbyněk <zbyneklazarek@gmail.com>
2023-08-14 10:10:54 +0200

    Get rid of Startup class and add authentication

---
+++ InterviewTestProject.TranslationManagement-master/TranslationManagement.Api/Auth/AuthConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace TranslationManagement.Api.Auth;
+
+public static class AuthConstants
+{
+    public const string AdminUserPolicyName = "Admin";
+    public const string AdminUserClaimName = "admin";
+
+    public const string ApiKeyHeaderName = "x-api-key";
+}
