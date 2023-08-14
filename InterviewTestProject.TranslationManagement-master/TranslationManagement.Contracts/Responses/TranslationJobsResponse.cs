05a30d7fe170dcd8348e5635d305193e81a72a65
Zbyněk <zbyneklazarek@gmail.com>
2023-08-14 09:31:17 +0200

    Contracts - create requests and responses

---
+++ InterviewTestProject.TranslationManagement-master/TranslationManagement.Contracts/Responses/TranslationJobsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslationManagement.Contracts.Responses;
+public class TranslationJobsResponse
+{
+    public required IEnumerable<TranslationJobResponse> Items { get; init; } = Enumerable.Empty<TranslationJobResponse>();
+}
---
+++ InterviewTestProject.TranslationManagement-master/TranslationManagement.Contracts/Responses/TranslatorModelsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslationManagement.Contracts.Responses;
+public class TranslatorModelsResponse
+{
+    public required IEnumerable<TranslatorModelResponse> Items { get; init; } = Enumerable.Empty<TranslatorModelResponse>();
+}
